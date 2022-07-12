ef61ff183628a9f1bd45ef81f4d9441cf5764c61
zemiles <abeb199309@gmail.com>
2022-07-12 09:08:05 +0900

    register.html 추가, 서버쪽 회원가입 완성

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://test:sparta@cluster0.rv5esal.mongodb.net/Cluster0?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://test:sparta@cluster0.ovlot.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
 # JWT 토큰을 만들 때 필요한 비밀문자열입니다. 아무거나 입력해도 괜찮습니다.
@@ -18,6 +18,29 @@ def home():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+#################################
+##  로그인을 위한 API            ##
+#################################
+
+
+# [회원가입 API]
+# id, pw, nickname을 받아서, mongoDB에 저장합니다.
+# 저장하기 전에, pw를 sha256 방법(=단방향 암호화. 풀어볼 수 없음)으로 암호화해서 저장합니다.
+@app.route('/api/register', methods=['POST'])
+def api_register():
+    id_receive = request.form['id_give']
+    pw_receive = request.form['pw_give']
+    pw_check_receive = request.form['pw_check_give']
+    gender_receive = request.form['gender_give']
+    nick_receive = request.form['nick_give']
+
+    print(id_receive,pw_receive,pw_check_receive,gender_receive,nick_receive)
+
+    db.users.insert_one({'id': id_receive, 'pw': pw_receive, 'pw_check': pw_check_receive,
+                         'gender': gender_receive, 'nick': nick_receive})
+    return jsonify({'result': 'success', 'msg': '회원가입을 완료했습니다.'})
 
 @app.route("/gyms", methods=["POST"])
 def web_mars_post():
